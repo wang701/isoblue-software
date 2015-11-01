78eacf92ac17713413c0de93dded022afc2b202e
Yang Wang <wang701@purdue.edu>
2015-11-01 14:56:10 -0500

    add raw CAN logger back and change makefile

--- tools/Android.mk
+++ tools/Android.mk
@@ -20,6 +20,20 @@ LOCAL_CFLAGS := $(PRIVATE_LOCAL_CFLAGS)
 include $(BUILD_EXECUTABLE)
 
 #
+# can_log_isobus
+#
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := can_log_isobus.c
+LOCAL_MODULE := can_log_isobus
+LOCAL_MODULE_TAGS := optional
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_CFLAGS := $(PRIVATE_LOCAL_CFLAGS)
+
+include $(BUILD_EXECUTABLE)
+
+#
 # can_log_raw
 #
 
@@ -32,6 +46,5 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
 LOCAL_CFLAGS := $(PRIVATE_LOCAL_CFLAGS)
 
 include $(BUILD_EXECUTABLE)
-
 # build cmd
 # ndk-build V=1 NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=./Android.mk NDK_APPLICATION_MK=./Application.mk
